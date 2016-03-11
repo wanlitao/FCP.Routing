e62657667a8751d463f13373ceb3e4216bc08f36
万里涛 <wanlitao_3055601@outlook.com>
2016-03-11 20:23:39 +0800

    modify comments

--- FCP.Routing.LoadBalance/Constants/LoadBalanceConstants.cs
+++ FCP.Routing.LoadBalance/Constants/LoadBalanceConstants.cs
@@ -1,12 +1,9 @@
 ﻿namespace FCP.Routing.LoadBalance
 {
-    /// <summary>
-    /// 负载均衡 常量
-    /// </summary>
     public static class LoadBalanceConstants
     {
         /// <summary>
-        /// 一致性Hash默认虚拟节点数
+        /// Consistent Hash default virtual Nodes
         /// </summary>
         public const int DefaultVirtualNodesFactor = 5;
     }
